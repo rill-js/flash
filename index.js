ef1c640b37238fb180cc94b2c3bb8b659aa91453
Dylan Piercey <pierceydylan@gmail.com>
2016-10-16 11:37:31 -0700

    Now saves flashed values for initial browser render

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var started = !process.browser
+
 /**
  * Middleware for creating flash messages in rill.
  *
@@ -8,12 +10,18 @@
  */
 module.exports = function flashSetup (opts) {
   opts = opts || {}
-  var namespace = 'key' in opts ? opts.key : '__rill_flash'
+  var namespace = 'key' in opts ? opts.key : '@rill/flash/'
+  var namespaceActive = namespace + 'active__'
+  var namespaceInitial = namespace + 'initial__'
 
   return function flashMiddleware (ctx, next) {
     // Check for rill session.
     var session = ctx.session
     if (!session) throw new Error('@rill/flash requires a session to work. Check out @rill/session.')
+
+    // Check if this is the initial render.
+    var isStarted = started
+    started = true
 
     /**
       * Flashes a value (using sessions) for the current request to the next one.
@@ -23,14 +31,35 @@ module.exports = function flashSetup (opts) {
         throw new TypeError('@rill/flash: Key must be a string.')
       }
 
-      key = namespace + '_' + key
+      var activeKey = namespaceActive + key
+      var initialKey = namespaceInitial + key
 
       if (arguments.length === 1) {
-        var result = session.get(key)
-        session.delete(key)
+        // Handle flash 'get'
+        var result = session.get(activeKey)
+
+        if (process.browser) {
+          // When in the browser we check if it is the initial render.
+          // The initial render will reuse expired flash data to facilitate bootstrapping.
+          if (!isStarted) {
+            result = result || session.get(initialKey)
+          }
+
+          // Then we delete the initial server cache.
+          session.delete(initialKey)
+        } else {
+          // In the server we store a cached value of the flash message for the initial render in the browser.
+          session.set(initialKey, result)
+        }
+
+        // Finally we delete the active key (since a flash is one use).
+        session.delete(activeKey)
+
+        // And send out the result.
         return result
       } else {
-        session.set(key, val)
+        // Handle flash 'set'
+        session.set(activeKey, val)
       }
     }
 
